877290dd4e732be71d5aaf93512cbc9356bd1702
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-08-12 19:34:30 +0500

    made the chat area and added the callback function to show the updated chat log

--- src/views/chat.jsx
+++ src/views/chat.jsx
@@ -8,7 +8,8 @@ const Api_URL = import.meta.env.VITE_API_URL;
 
 export default function ChatArea(){
     const {id} = useParams();
-    const [data, setData] = useState({success : false , data : [] , my_id : '' ,  user : []});
+    const [data, setData] = useState({success : false , data : [] , my_id : '' ,  user : {}});
+     const [message , setMessage] = useState('')
     const navigate = useNavigate();
     
 
@@ -56,6 +57,71 @@ export default function ChatArea(){
  
     }, [id , navigate])
 
+
+
+async function SendMsg (){
+   
+    
+   
+    const url =  `${Api_URL}/messages/dm/${id} `
+
+    try{
+            const response = await fetch(url , {
+                method : 'POST',
+                headers: {
+                "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+                "Content-Type": "application/json"
+            },
+                body : JSON.stringify({'msg_content' : message})
+            })
+
+            const result = await response.json();
+
+            if(!response.ok){
+                throw new Error(result.error || 'Error');
+            }
+            
+            console.log(result.message)
+            
+            if(result.data){
+                setData((prev)=>({
+                   ...prev , 
+                   data : [...prev.data , result.data]
+                }))
+            }
+
+          setMessage('')
+ 
+        }
+        catch (error){
+            console.log(error.message);
+            
+            
+        }
+        
+ }
+
+
+ const handleKeyDown = (e) => {
+
+  if (e.key === 'Enter' && !e.shiftKey) {
+    e.preventDefault(); 
+    
+    if (message.trim()) {
+      SendMsg(e); 
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
     return (
        <>
       <div className="flex flex-col overflow-hidden w-full h-full">
@@ -66,16 +132,16 @@ export default function ChatArea(){
          </div>
        <div className=" flex flex-1 w-full min-h-0 overflow-hidden">
         <div className="flex   flex-1  min-h-0 overflow-hidden flex-col  ">
-            <div className=" flex flex-col  w-full min-h-0 flex-1 overflow-y-auto min-w-50 p-2 space-y-3 mb-2">
-            { data.data.map((msg)=>{
+            <div className=" flex flex-col  w-full min-h-0 flex-1 overflow-y-auto overflow-hidden min-w-50 p-2 space-y-3 mb-2">
+            { data.data.map((msg , index)=>{
               const isMe = msg.sender_id === data.my_id 
-              console.log(isMe)
+              
 
               return(
                 
-                <div key={msg.id} className={`flex w-full  items-start ${isMe ? 'justify-end ' : 'justify-start '} `} > 
+                <div key={msg._id || msg.message_id || `msg-${index}`} className={`flex w-full  items-start ${isMe ? 'justify-end ' : 'justify-start '} `} > 
 
-                   <div className=" p-1 border rounded-xl text-sm flex-none max-w-[70%]  break-words shadow-sm leading-relaxed ">{msg.content}</div> 
+                   <div className={` px-3 py-1  rounded-2xl text-sm flex-none max-w-[70%]  break-words shadow-sm leading-relaxed ${isMe ? 'bg-green-700/50 ' : 'bg-zinc-500/50 '} `}>{msg.content}</div> 
                 </div>
                 
                 
@@ -89,8 +155,16 @@ export default function ChatArea(){
 
 
          <div className="flex w-full p-1 pl-2 border shrink-0 rounded-3xl items-center ">
-            <textarea rows={'2'} className="flex-1 h-6 border-none outline-none resize-none bg-transparent w-full overflow-hidden ml-2" />
-            <button className="h-7 w-15 bg-green-400/50 mr-2 ml-2 rounded-2xl">Send</button>
+            <textarea 
+            rows={'2'} 
+            className="flex-1 h-6 border-none outline-none resize-none bg-transparent w-full overflow-hidden ml-2" 
+            onKeyDown={handleKeyDown} 
+            value={message}
+            onChange={(e)=> {
+                setMessage(e.target.value)
+                }}/>
+                
+            <button className="h-7 w-15 bg-green-400/50 mr-2 ml-2 rounded-2xl transition-colors disabled:bg-zinc-800 disabled:text-zinc-500 disabled:cursor-not-allowed disabled:hover:bg-zinc-800" onClick= {SendMsg}  disabled={!message.trim()}>Send</button>
          </div>
 
         </div>
